79cd7da9a05357e99d866e56bfb4b9f061b47588
xli52 <lxmichael52@gmail.com>
2022-06-23 10:53:51 -0700

    minor change

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -32,10 +32,12 @@ describe('Form', () => {
   it("validates that the student name is not blank", () => {
     /* 1. Create the mock onSave function */
     const onSave = jest.fn();
+
     /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the student prop should be blank or undefined */
     const { getByText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
+    
     /* 3. Click the save button */
     fireEvent.click(getByText('Save'))
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
@@ -45,6 +47,7 @@ describe('Form', () => {
   it("validates that the interviewer cannot be null", () => {
     /* 1. Create the mock onSave function */
     const onSave = jest.fn();
+
     /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the interviewer prop should be null */
     const { getByText } = render(
       <Form
